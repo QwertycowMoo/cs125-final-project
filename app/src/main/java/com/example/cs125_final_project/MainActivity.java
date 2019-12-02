8e879ea82db68f7ee5bd2838cecc951f2a2a7130
Kevin Zhou <kjzhou2@illinois.edu>
2019-12-02 16:54:44 -0600

    Started TrophyActivity and Credits

--- app/src/main/java/com/example/cs125_final_project/MainActivity.java
+++ app/src/main/java/com/example/cs125_final_project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.cs125_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnGame = (Button) findViewById(R.id.btnArcadeMode);
+        Button btnTrophyRoom = (Button) findViewById(R.id.btnTrophyRoom);
+        Button btnCredits = (Button) findViewById(R.id.btnCredits);
+        btnTrophyRoom.setOnClickListener(v -> {
+            Intent trophyRoomIntent = new Intent();
+            
+        });
     }
 }
