b49b33f38a0a63e1066b6fb57b7d1bbab4dfc333
Kevin Zhou <kjzhou2@illinois.edu>
2019-12-04 19:27:09 -0600

    Started the ClickLogo Game, need to rename the java class

---
+++ app/src/main/java/com/example/cs125_final_project/BonusCoinsGame.java
@@ -0,0 +1,15 @@
+package com.example.cs125_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BonusCoinsGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+    }
+}
